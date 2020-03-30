0f4f6259a1fb1f51460dfb389dcb6e7d7e97c29b
chetat <yekuwilfred@gmail.com>
2020-03-30 06:59:39 +0100

    Implement CRUD endpoints for Tickets

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -2,4 +2,4 @@ from flask import Blueprint
 
 api = Blueprint('api', __name__, url_prefix='/api/v1')
 
-from . import events, event_type
+from . import events, event_type, tickets
---
+++ app/api/tickets.py
@@ -0,0 +1,83 @@
+from app.api import api
+from flask import jsonify, request
+from models import db, Tickets
+from Exceptions import NotFound, MethodNotAllowed, \
+    Forbiden, InternalServerError, ExistingResource, AuthError
+from .authhelpers import requires_auth
+
+
+@api.errorhandler(NotFound)
+@api.errorhandler(Forbiden)
+@api.errorhandler(MethodNotAllowed)
+@api.errorhandler(InternalServerError)
+@api.errorhandler(ExistingResource)
+def api_error(error):
+    payload = dict(error.payload or ())
+    payload['code'] = error.status_code
+    payload['message'] = error.message
+    payload['success'] = error.success
+    return jsonify(payload), error.status_code
+
+
+@api.route('events/tickets')
+def get_tickets():
+    try:
+        tickets = Tickets.query.all()
+        return jsonify({
+            "success": True,
+            "data": [ticket.serialize for ticket in tickets]
+            })
+    except Exception as e:
+        raise InternalServerError("Failed to fetch tickets")
+
+
+@api.route('events/tickets', methods=["POST"])
+def new_ticket():
+    event_id = request.get_json()["event_id"]
+    attender_email = request.get_json()["email"]
+
+    try:
+        new_ticket = Tickets(event_id=event_id, attender_email=attender_email)
+        db.session.add(new_ticket)
+        db.session.commit()
+        return jsonify({
+            "success": True,
+            "data": new_ticket.serialize
+        })
+    except Exception as e:
+        print(e)
+        db.session.rollback()
+        raise InternalServerError("Could not create new event ticket")
+
+
+@api.route('events/tickets/<ticket_id>')
+def get_ticket(ticket_id):
+    try:
+        ticket = Tickets.query.filter_by(id=ticket_id).first()
+    except Exception as e:
+        print(e)
+        db.session.rollback()
+        raise InternalServerError("Failed to fetch ticket")
+    if not ticket:
+        raise NotFound(f"Ticket with id {ticket_id} not found")
+    else:
+        return jsonify(ticket.serialize)
+
+
+@api.route('events/tickets/<ticket_id>', methods=["DELETE"])
+def cancel_ticket(ticket_id):
+    try:
+        ticket = Tickets.query.filter_by(id=ticket_id).first()
+    except Exception as e:
+        print(e)
+        db.session.rollback()
+        raise InternalServerError("Failed to fetch ticket")
+    if not ticket:
+        raise NotFound(f"Ticket with id {ticket_id} not found")
+    else:
+        db.session.delete(ticket)
+        db.session.commit()
+        return jsonify({
+            "success": True,
+            "deleted_id": ticket_id
+        })
